21dde7b9001da853e3a2f3424ea991623acab2b7
Jaime Arturo Gomez <jaimegomez27@gmail.com>
2016-04-25 10:24:18 -0600

    updated to more elegant version of the code

--- PlotAdder.C
+++ PlotAdder.C
@@ -2,44 +2,47 @@
 
   char histname[128];
   char histoname[128];
-  TFile *f = new TFile("jgomez2-dflt-tag-sum.root");
-  f->cd("histos/TwoDetectorCoinc");
+  _file0->cd("histos/DoubleCoinc");
 
+  sprintf(histoname,"dc1_3_diff_time_t00_ppac01_ligl01");
+  TH1 *hf = (TH1*)gDirectory->FindObjectAny(histoname);
 
   for (int l=1;l<23;l++)
     {
-      if (l<10) sprintf(histoname,"td0_05_tdi2_ppac01_ligl0%i_int1",l);
-      if (l>=10) sprintf(histoname,"td0_05_tdi2_ppac01_ligl%i_int1",l);
-      TH2 *h1 = (TH2*)gDirectory->FindObjectAny(histoname);
-      
-        for (int i=2;i<3;i++)
-          {
-            if (l<10)
-              {
-                if (i<10) sprintf(histname,"td0_05_tdi2_ppac0%i_ligl0%i_int1",i,l);
-                if (i=10) sprintf(histname,"td0_05_tdi2_ppac%i_ligl0%i_int1",i,l);
-              }
-            if (l>=10)
-              {
-                if (i<10) sprintf(histname,"td0_05_tdi2_ppac0%i_ligl%i_int1",i,l);
-                if (i=10) sprintf(histname,"td0_05_tdi2_ppac%i_ligl%i_int1",i,l);
-              }
-            TH2 *h = (TH2*)gDirectory->FindObjectAny(histname);
-            int binsX=h->GetXaxis()->GetNbins();
-            int binsY=h->GetYaxis()->GetNbins();
+      if (l == 3 ||
+          l == 5 ||
+          l == 16) continue;
+      for (int i=1;i<11;i++)
+        {
+          if (l==1 && i==1) continue;
+          // std::cout<<"LIGL NUMBER "<<l<<"   "<<"PPAC NUMBER "<<i-1<<std::endl;                                             
+          sprintf(histname,"dc1_3_diff_time_t00_ppac%02i_ligl%02i",i,l);
+          TH1 *h = (TH1*)gDirectory->FindObjectAny(histname);
+          hf->Add(h);
+        }//end of loop over ppacs                                                                                             
+    }//end of loop over ligls                                                                                                 
+  new TCanvas;
+  hf->SetLineColor(kRed);
+  hf->Draw();
 
-            /* for (int j=1;j<binsY+1;j++)
-               {
-               for (int k=1;k<binsX+1;k++)
-               {
-               h1->AddBinContent(h1->GetBin(k,j),h->GetBinContent(k,j));
-               }//end of loop over x bins
-               }//end of loop over y bins*/
-            h1->Add(h);
-          }//end of loop over ppacs
+  _file0->cd("histos/TwoDetectorCoinc/background");
+  sprintf(histoname,"td1_05_back_time_ppac01_ligl01");
+  TH1* hb = (TH1*)gDirectory->FindObjectAny(histoname);
 
-      new TCanvas;
-      gPad->SetLogz();
-      h1->Draw("colz");
-    }//end of loop over ligls
-}//end of the whole thing
+  for (int l=1;l<23;l++)
+    {
+      if (l == 3 ||
+          l == 5 ||
+          l == 16) continue;
+      for (int i=1;i<11;i++)
+        {
+          if (l==1 && i==1) continue;
+          sprintf(histname,"td1_05_back_time_ppac%02i_ligl%02i",i,l);
+          TH1 *h = (TH1*)gDirectory->FindObjectAny(histname);
+          hb->Add(h);
+        }//end of loop over ppacs                                                                                             
+    }//end of loop over ligls                                                                                                 
+  hb->Draw("same");
+
+
+}//end of the whole thing  
