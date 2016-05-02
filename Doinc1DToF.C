1178028d53499d866e18e9e29648a0d84673766f
Jaime Arturo Gomez <jaimegomez27@gmail.com>
2016-05-02 10:18:19 -0600

    Update Doinc1DToF.C

--- Doinc1DToF.C
+++ Doinc1DToF.C
@@ -6,16 +6,15 @@ char histoname[128];
 char histname[128];
 char histtitle[128];
 char canvasname[128];
-TCanvas *myCanvas[22];
+TCanvas *myCanvas[10];
 
 
   TFile *f = TFile::Open("FinalSum.root","READ");
 
 Int_t IntegralPlotter(){
 
-  for (int i = 1; i<23; i++)
+  for (int i = 1; i<11; i++)
     {
-      if (i==3) continue;
       findHisto(i);
     }
   return 0;
@@ -23,30 +22,28 @@ Int_t IntegralPlotter(){
 
 
 
-void findHisto(int ligl) {
+void findHisto(int ppac) {
 
 
-  sprintf(canvasname,"ligl%02i",ligl);
-  myCanvas[ligl-1] = new TCanvas(canvasname,canvasname,1000,700);
-  myCanvas[ligl-1]->Divide(2,1);
+  sprintf(canvasname,"ppac%02i",ppac);
+  myCanvas[ppac-1] = new TCanvas(canvasname,canvasname,1000,700);
 
-  f->cd("histos/Integrals");
-  sprintf(histoname,"i0_ligl%02i_integral1_baseRestored",ligl);
-  TH1* h = (TH1*)gDirectory->FindObjectAny(histoname);
-  myCanvas[ligl-1]->cd(1);
+  f->cd("histos/DoubleCoinc");
+  for (int j=1;j<23;j++)
+    {
+      sprintf(histoname,"dc1_5_diff_time_t00_ppac%02i_ligl%02i",ppac,j);
+      if (j==1) TH1 *h = (TH1*)gDirectory->FindObjectAny(histoname);
+      if (j==3 || j==5 || j==16) continue;
+      TH1*  h = (TH1*)gDirectory->FindObjectAny(histoname);
+      hf->Add(h);
+    }//end of loop over ligls
+    
+  gPad->SetLogy(1);
   sprintf(histotitle,"Ligl %02i Integral 1 Raw",ligl);
-  h->SetTitle(histotitle);
+  hf->SetTitle(histotitle);
   gStyle->SetOptStat(0);
-  h->Draw();
+  hf->Draw();
 
-  f->cd("histos/IntegralsCleaned");
-  sprintf(histname,"i1_ligl%02i_integral1_baseRestored",ligl);
-  TH1* h1 = (TH1*)gDirectory->FindObjectAny(histname);
-  myCanvas[ligl-1]->cd(2);
-  sprintf(histtitle,"Ligl %02i Integral 1 Cleaned",ligl);
-  h1->SetTitle(histtitle);
-  gStyle->SetOptStat(0);
-  h1->Draw();
 
 }//end of findHisto                                                                                                           
 
