0061ad4ef7f2c24ee989a7960326c7b9e9a5f611
Jaime Arturo Gomez <jaimegomez27@gmail.com>
2016-05-02 09:48:19 -0600

    Update IntegralPlotter.C

--- IntegralPlotter.C
+++ IntegralPlotter.C
@@ -1,23 +1,29 @@
 void findHisto(int);
+
+char histotitle[128];
 char histoname[128];
 char histname[128];
+char histtitle[128];
 char canvasname[128];
 TCanvas *myCanvas[22];
 
-Int_t IntegralPlotter(){
-TFile *f = TFile::Open("FinalSum.root","READ");
 
-for (int i = 0; i<23; i++)
-  {
-    if (i==3) continue;
-    findHisto(i);
-  }
-return 0;
+  TFile *f = TFile::Open("FinalSum.root","READ");
+
+Int_t IntegralPlotter(){
+
+  for (int i = 1; i<23; i++)
+    {
+      if (i==3) continue;
+      findHisto(i);
+    }
+  return 0;
 }
 
 
 
 void findHisto(int ligl) {
+
 
   sprintf(canvasname,"ligl%02i",ligl);
   myCanvas[ligl-1] = new TCanvas(canvasname,canvasname,1000,700);
@@ -27,12 +33,18 @@ void findHisto(int ligl) {
   sprintf(histoname,"i0_ligl%02i_integral1_baseRestored",ligl);
   TH1* h = (TH1*)gDirectory->FindObjectAny(histoname);
   myCanvas[ligl-1]->cd(1);
+  sprintf(histotitle,"Ligl %02i Integral 1 Raw",ligl);
+  h->SetTitle(histotitle);
+  gStyle->SetOptStat(0);
   h->Draw();
-  
+
   f->cd("histos/IntegralsCleaned");
   sprintf(histname,"i1_ligl%02i_integral1_baseRestored",ligl);
   TH1* h1 = (TH1*)gDirectory->FindObjectAny(histname);
   myCanvas[ligl-1]->cd(2);
+  sprintf(histtitle,"Ligl %02i Integral 1 Cleaned",ligl);
+  h1->SetTitle(histtitle);
+  gStyle->SetOptStat(0);
   h1->Draw();
-  
-}//end of findHisto
+
+}//end of findHisto    
