1992bed0d55550258a60f5311427a24992f5e779
Jaime Arturo Gomez <jaimegomez27@gmail.com>
2016-05-02 09:34:58 -0600

    Create IntegralPlotter.C

---
+++ IntegralPlotter.C
@@ -0,0 +1,40 @@
+void findHisto(int);
+
+Int_t IntegralPlotter(){
+
+char histoname[128];
+char histname[128];
+char canvasname[128];
+TCanvas *myCanvas[22];
+
+TFile *f = TFile::Open("FinalSum.root","READ");
+
+for (int i = 0; i<23; i++)
+  {
+    if (i==3) continue;
+    findHisto(i);
+  }
+return 0;
+}
+
+
+
+void findHisto(int ligl) {
+
+  sprintf(canvasname,"ligl%02i",ligl);
+  myCanvas[ligl-1] = new TCanvas(canvasname,canvasname,1000,700);
+  myCanvas[ligl-1]->Divide(2,1);
+
+  f->cd("histos/Integrals");
+  sprintf(histoname,"i0_ligl%02i_integral1_baseRestored",ligl);
+  TH1* h = (TH1*)gDirectory->FindObjectAny(histoname);
+  myCanvas[ligl-1]->cd(1);
+  h->Draw();
+  
+  f->cd("histos/IntegralsCleaned");
+  sprintf(histname,"i1_ligl%02i_integral1_baseRestored",ligl);
+  TH1* h1 = (TH1*)gDirectory->FindObjectAny(histname);
+  myCanvas[ligl-1]->cd(2);
+  h1->Draw();
+  
+}//end of findHisto
