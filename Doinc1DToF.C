0e355931b5c2c4b329ef96bd8458a31d8e7e565b
Jaime Arturo Gomez <jaimegomez27@gmail.com>
2016-05-03 14:39:13 -0600

    Update Doinc1DToF.C

--- Doinc1DToF.C
+++ Doinc1DToF.C
@@ -27,31 +27,41 @@ void findHisto(int ppac) {
   sprintf(canvasname,"ppac%02i",ppac);
   myCanvas[ppac-1] = new TCanvas(canvasname,canvasname,1000,700);
 
-  f->cd("histos/DoubleCoinc");
+
   for (int j=1;j<23;j++)
     {
-      sprintf(histoname,"dc1_7_diff_time_t00_ppac%02i_ligl%02i",ppac,j);
-      if (j==1) TH1 *hf = (TH1*)gDirectory->FindObjectAny(histoname);
+      f->cd("histos/DoubleCoinc");
+      sprintf(histoname,"dc1_5_diff_time_t00_ppac%02i_ligl%02i",ppac,j);
       if (j==3 || j==5 || j==16) continue;
-      TH1*  h = (TH1*)gDirectory->FindObjectAny(histoname);
+      if (j==1) TH1 *hf = (TH1*)gDirectory->FindObjectAny(histoname);
+      else      TH1*  h = (TH1*)gDirectory->FindObjectAny(histoname);
       hf->Add(h);
-      //std::cout<<"Adding "<<histoname<<std::endl;                                                                           
-    }//end of loop over ligls                                                                                                 
+      //std::cout<<"Adding "<<histoname<<std::endl;                                                                          \
+                                                                                                                              
+      f->cd("histos/DoubleCoinc/background");
+      sprintf(histname,"dc1_5_back_diff_time_t00_ppac%02i_ligl%02i",ppac,j);
+      if (j==3 || j==5 || j==16) continue;
+      if (j==1) TH1 *hb = (TH1*)gDirectory->FindObjectAny(histoname);
+      else      TH1* h1 = (TH1*)gDirectory->FindObjectAny(histoname);
+      hb->Add(h1);
+    }//end of loop over ligls                                                                                                \
+                                                                                                                              
 
   myCanvas[ppac-1]->cd(1);
-  //gPad->SetLogy();                                                                                                          
+  //gPad->SetLogy();                                                                                                         \
+                                                                                                                              
   gPad->SetLogy();
   sprintf(histotitle,"ToF PPAC %02i All Ligls Except 3,5,16",ppac);
   hf->SetTitle(histotitle);
   gStyle->SetOptStat(0);
   hf->Draw();
+  hb->Draw("same");
 
-  tex1=new TLatex(0.65,0.75,"E_{n}^{in} = 12-18 MeV");
+  tex1=new TLatex(0.65,0.75,"E_{n}^{in} = 0.7-6 MeV");
   tex1->SetNDC();
   tex1->SetTextFont(43);
   tex1->SetTextSize(22);
   tex1->SetTextColor(kRed);
   tex1->Draw();
 
-}//end of findHisto                                                                                                           
-
+}//end of findHisto      
